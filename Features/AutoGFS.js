1cfe03852b625e28ece28cffc8d272ab1822c620
GreenDev556 <87093775+GreenDev556@users.noreply.github.com>
2024-06-25 21:07:29 +0100

    Update AutoGFS.js

--- Features/AutoGFS.js
+++ Features/AutoGFS.js
@@ -3,7 +3,7 @@ import Config from "../Config";
 register("tick", () => {
     if (Config.enableAutoGfs !== true) return
     if (Player.getInventory().getItems().some(a => a?.getName()?.removeFormatting() === "Ender Pearl") == false) {
-        ChatLib.command("od ep")
+        ChatLib.command("gfs ender pearl 16")
     }
 })
 
