2ff28f3c886ef9b04b0d454f7711d90eeba371d4
x45k <michaelbrennom@gmail.com>
2024-06-25 07:11:03 -0500

    green do your job

--- Features/RngAnnounce.js
+++ Features/RngAnnounce.js
@@ -2,7 +2,10 @@ import Config from "../Config";
 
 register("chat", (rng, floor) => {
     if (!(Config.enableRngAnnounce)) return
-    ChatLib.command(`pc I just dropped ${rng}!`)
-    if (!(Config.enableGuildRngAnnounce)) return
-    ChatLib.command(`gc I just dropped ${rng} from ${floor}!`)
+    setTimeout(() => {
+        ChatLib.command(`pc I just dropped ${rng}!`)
+        Thread.sleep(500)
+        if (!(Config.enableGuildRngAnnounce)) return
+        ChatLib.command(`gc I just dropped ${rng} from ${floor}!`)
+    }, 750)
 }).setCriteria("RNG METER! Reselected the ${rng} for Catacombs - ${floor}! CLICK HERE to select a new drop!")
--- Features/loading.js
+++ Features/loading.js
@@ -3,5 +3,5 @@ import Config from "../Config";
 register("worldLoad", () => {
     if (!Config.enableloadingmessages) return
     ChatLib.chat("&2[WEED CLIENT] &7loading...");
-    ChatLib.chat("&2[WEED CLIENT] &7loaded!");
+    setTimeout(() => {ChatLib.chat("&2[WEED CLIENT] &7loaded!")}, 500)
 });
